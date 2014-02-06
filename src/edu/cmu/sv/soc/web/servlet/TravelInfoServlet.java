5f6d051457c4af725f4b8e031b808639cfb646ca
Surya Kiran <suryakiranl@gmail.com>
2014-02-06 09:27:52 -0800

    Formatted code and added method in and out DEBUG logs

--- src/edu/cmu/sv/soc/web/servlet/TravelInfoServlet.java
+++ src/edu/cmu/sv/soc/web/servlet/TravelInfoServlet.java
@@ -11,12 +11,7 @@ import org.apache.log4j.Logger;
 
 import edu.cmu.sv.soc.dto.TravelSearchRequest;
 import edu.cmu.sv.soc.dto.TravelSearchResponse;
-import edu.cmu.sv.soc.dto.WeatherAPIResponse;
-import edu.cmu.sv.soc.dto.gson.CurrentConditionGson;
-import edu.cmu.sv.soc.dto.gson.MsgGson;
-import edu.cmu.sv.soc.dto.gson.WeatherRequestGson;
 import edu.cmu.sv.soc.dto.gson.travel.AirlineGson;
-import edu.cmu.sv.soc.dto.gson.travel.AirportGson;
 import edu.cmu.sv.soc.dto.gson.travel.RouteGson;
 import edu.cmu.sv.soc.dto.gson.travel.StopGson;
 import edu.cmu.sv.soc.service.ITravel;
@@ -27,61 +22,56 @@ import edu.cmu.sv.soc.service.Rome2RioTravelImpl;
  */
 public class TravelInfoServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private static final Logger log = Logger.getLogger(WeatherInfoServlet.class);
-	
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public TravelInfoServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	private static final Logger log = Logger
+			.getLogger(WeatherInfoServlet.class);
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public TravelInfoServlet() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		log.debug("Inside doGet method");
+
 		String startLocation = request.getParameter("startLocation");
-		String destination 	 = request.getParameter("destination");
+		String destination = request.getParameter("destination");
 
 		TravelSearchResponse travel = null;
 		ITravel ts = new Rome2RioTravelImpl();
-		if(startLocation != null && destination != null) {
-			TravelSearchRequest pathRequest = new TravelSearchRequest(startLocation, destination);
+		if (startLocation != null && destination != null) {
+			TravelSearchRequest pathRequest = new TravelSearchRequest(
+					startLocation, destination);
 			travel = ts.findTravelPaths(pathRequest);
 		}
-		
-		if(travel == null) {
-			throw new RuntimeException("Error when retrieving travel information.");
+
+		if (travel == null) {
+			throw new RuntimeException(
+					"Error when retrieving travel information.");
 		}
-		
+
 		StringBuffer sb = new StringBuffer();
-		if( travel.getRoutes() != null ) {
-			sb.append(prepareCurrentStatus(travel));	
+		if (travel.getRoutes() != null) {
+			sb.append(prepareCurrentStatus(travel));
 		}
-		
+
 		response.setCharacterEncoding("UTF-8");
 		response.setContentType("text/plain");
 		response.getWriter().write(sb.toString());
 		response.flushBuffer();
-		
+
+		log.debug("Exiting doGet method");
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		log.debug("Inside doPost method");
 		doGet(request, response);
-		
+		log.debug("Exiting doPost method");
 	}
 
-	/**
-	 * @param travel - TravelSearchResponse
-	 */	
 	private String prepareCurrentStatus(TravelSearchResponse travel) {
 		StringBuffer sb = new StringBuffer();
-		
+
 		RouteGson[] routes = travel.getRoutes();
 		StopGson[] stops = routes[0].getStops();
 		AirlineGson[] airline = travel.getAirlines();
@@ -90,16 +80,20 @@ public class TravelInfoServlet extends HttpServlet {
 		sb.append("<hr/>");
 		sb.append("<div class='container'>");
 		sb.append("<div class='row'><div class='col-md-2'>");
-		sb.append("Distance: </div><div class='col-md-2'>" + routes[0].getDistance() + " miles");
+		sb.append("Distance: </div><div class='col-md-2'>"
+				+ routes[0].getDistance() + " miles");
 		sb.append("</div></div><div class='row'><div class='col-md-2'>");
-		sb.append("Duration: </div><div class='col-md-2'>" + routes[0].getDuration() + " minutes");
+		sb.append("Duration: </div><div class='col-md-2'>"
+				+ routes[0].getDuration() + " minutes");
 		sb.append("</div></div><div class='row'><div class='col-md-2'>");
 		sb.append("Stops: </div><div class='col-md-2'>" + stops[0].getName());
 		sb.append("</div></div><div class='row'><div class='col-md-2'>");
 		sb.append("</div></div><div class='row'><div class='col-md-2'>");
-		sb.append("Airline: </div><div class='col-md-2'>" + airline[0].getName());
+		sb.append("Airline: </div><div class='col-md-2'>"
+				+ airline[0].getName());
 		sb.append("</div></div><div class='row'><div class='col-md-2'>");
 		sb.append("</div></div></div>");
+
 		return sb.toString();
 	}
 
--- src/edu/cmu/sv/soc/web/servlet/WeatherInfoServlet.java
+++ src/edu/cmu/sv/soc/web/servlet/WeatherInfoServlet.java
@@ -22,95 +22,101 @@ import edu.cmu.sv.soc.service.WorldWeatherOnlineImpl;
 
 public class WeatherInfoServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private static final Logger log = Logger.getLogger(WeatherInfoServlet.class);
-	
-	/**
-     * @see HttpServlet#HttpServlet()
-     */
-    public WeatherInfoServlet() {
-        super();
-    }
+	private static final Logger log = Logger
+			.getLogger(WeatherInfoServlet.class);
 
-    /**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {		
+	public WeatherInfoServlet() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		log.debug("Inside doGet method");
 		String cityNameorZip = request.getParameter("cityNameOrZip");
 		String numberOfDays = request.getParameter("days");
-		
+
 		WeatherAPIResponse weather = null;
 		IWeather ws = new WorldWeatherOnlineImpl();
-		
-		if(numberOfDays == null || "".equals(numberOfDays.trim())) {
+
+		if (numberOfDays == null || "".equals(numberOfDays.trim())) {
 			weather = ws.getWeatherInfo(cityNameorZip);
 		} else {
 			int days = Integer.parseInt(numberOfDays);
 			weather = ws.getWeatherInfo(cityNameorZip, days);
 		}
-		
-		if(weather == null) {
-			throw new RuntimeException("Error when retrieving weather information.");
+
+		if (weather == null) {
+			throw new RuntimeException(
+					"Error when retrieving weather information.");
 		}
-		
+
 		StringBuffer sb = new StringBuffer();
-		if( weather.getData().getCurrent_condition() != null ) {
-			sb.append(prepareCurrentCondition(weather));	
+		if (weather.getData().getCurrent_condition() != null) {
+			sb.append(prepareCurrentCondition(weather));
 		} else {
 			MsgGson[] messg = weather.getData().getError();
-			if(messg == null) {
+			if (messg == null) {
 				sb.append("<b>No information/message obtained from weather server.</b>");
 			} else {
 				sb.append("<b>" + messg[0].getMsg() + "</b>");
 			}
 		}
-		
+
 		response.setCharacterEncoding("UTF-8");
 		response.setContentType("text/plain");
 		response.getWriter().write(sb.toString());
 		response.flushBuffer();
-		
+
+		log.debug("Exiting doGet method");
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-		response.getWriter().write("POST method is not implemented for this Servlet. Please use GET.");
-	
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		log.debug("Inside doPost method");
+		response.getWriter()
+				.write("POST method is not implemented for this Servlet. Please use GET.");
+		log.debug("Exiting doPost method");
 	}
-	
-	/**
-	 * @param weather - WeatherAPIResponse
-	 */	
+
 	private String prepareCurrentCondition(WeatherAPIResponse weather) {
 		StringBuffer sb = new StringBuffer();
-		
+
 		WeatherRequestGson req = weather.getData().getRequest()[0];
 		CurrentConditionGson cc = weather.getData().getCurrent_condition()[0];
 
-		sb.append("Displaying weather information for " + req.getType() + " : <strong>" + req.getQuery() + "</strong>");
+		sb.append("Displaying weather information for " + req.getType()
+				+ " : <strong>" + req.getQuery() + "</strong>");
 		sb.append("<hr/>");
 		sb.append("<strong>Weather Information:</strong>");
 
 		sb.append("<div class='container'>");
 		sb.append("<div class='row'><div class='col-md-2'>");
-		sb.append("Reading recorded at: </div><div class='col-md-2'>" + cc.getObservation_time() + " (Local Time)");
+		sb.append("Reading recorded at: </div><div class='col-md-2'>"
+				+ cc.getObservation_time() + " (Local Time)");
 		sb.append("</div></div><div class='row'><div class='col-md-2'>");
-		sb.append("Temperature: </div><div class='col-md-2'>" + cc.getTemp_F() + "F / " + cc.getTemp_C() + "C");
+		sb.append("Temperature: </div><div class='col-md-2'>" + cc.getTemp_F()
+				+ "F / " + cc.getTemp_C() + "C");
 		sb.append("</div></div><div class='row'><div class='col-md-2'>");
-		sb.append("Cloud Cover: </div><div class='col-md-2'>" + cc.getCloudcover() + "%");
+		sb.append("Cloud Cover: </div><div class='col-md-2'>"
+				+ cc.getCloudcover() + "%");
 		sb.append("</div></div><div class='row'><div class='col-md-2'>");
-		sb.append("Humidity: </div><div class='col-md-2'>" + cc.getHumidity() + "%");
+		sb.append("Humidity: </div><div class='col-md-2'>" + cc.getHumidity()
+				+ "%");
 		sb.append("</div></div><div class='row'><div class='col-md-2'>");
-		sb.append("Weather Description: </div><div class='col-md-2'>" + cc.getWeatherDesc()[0].getValue() );
+		sb.append("Weather Description: </div><div class='col-md-2'>"
+				+ cc.getWeatherDesc()[0].getValue());
 		sb.append("</div></div><div class='row'><div class='col-md-2'>");
-		sb.append("Wind Speed: </div><div class='col-md-2'>" + cc.getWindspeedMiles() + "mph");
+		sb.append("Wind Speed: </div><div class='col-md-2'>"
+				+ cc.getWindspeedMiles() + "mph");
 		sb.append("</div></div><div class='row'><div class='col-md-2'>");
-		sb.append("Wind direction: </div><div class='col-md-2'>" + cc.getWinddir16Point());
+		sb.append("Wind direction: </div><div class='col-md-2'>"
+				+ cc.getWinddir16Point());
 		sb.append("</div></div><div class='row'><div class='col-md-2'>");
-		sb.append("Weather Icon: </div><div class='col-md-2'><img src='" + cc.getWeatherIconUrl()[0].getValue() + "' class='img-responsive' alt='Responsive image'/>");
+		sb.append("Weather Icon: </div><div class='col-md-2'><img src='"
+				+ cc.getWeatherIconUrl()[0].getValue()
+				+ "' class='img-responsive' alt='Responsive image'/>");
 		sb.append("</div></div></div>");
+
 		return sb.toString();
 	}
 
