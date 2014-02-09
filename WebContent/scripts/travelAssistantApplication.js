d7965ec81931fa8b2215ec91dd6316f5cad6c9fd
Shishir Kinkar <shishir.kinkar@gmail.com>
2014-02-09 01:39:16 -0800

    UI updates. Ajax loader added.

--- WebContent/scripts/travelAssistantApplication.js
+++ WebContent/scripts/travelAssistantApplication.js
@@ -1,8 +1,10 @@
-	$(document).ready(function (){
+		// Trigger Tabs	
+$(document).ready(function (){
 		$("#topTabs").click(function(){
 			$(this).tab('show');
 		});
 
+		// Ajax query for weather
 		$("#getWeatherInfo").on('click', function(e){
 			e.preventDefault();
 			var cityNameorZipCode = $("#loc").val();
@@ -15,9 +17,18 @@
     			},
     			error: function(request, errorType, errorMessage){
     				alert('Error: ' + errorType + " with message " + errorMessage); },
+    			beforeSend: function(){
+    				$("#ajaxLoader").show();
+    			},
+    			complete: function() {
+    				$("#ajaxLoader").hide();
+    				$("#weatherDisplay").css('display', 'block');
+    			},
     			});
     	});
+
 		
+		// Ajax query for travel		
 		$("#getTravelInfo").on('click', function(e){
 			e.preventDefault();
 			var fromLoc = $("#startLoc").val();
@@ -31,6 +42,23 @@
     			},
     			error: function(request, errorType, errorMessage){
     				alert('Error: ' + errorType + " with message " + errorMessage); },
+        			beforeSend: function(){
+        				$("#ajaxLoaderTravel").show();
+        			},
+        			complete: function() {
+        				$("#ajaxLoaderTravel").hide();
+        				$("#travelDisplay").css('display', 'block');
+        			},
     			});
     	});
+		
+		$("#resetTravelInfo").click(function() {
+			$("#startLoc, #destLoc").val("");
+			$("#travelDisplay").css('display', 'none');
+		});
+		
+		$("#resetWeatherInfo").click(function() {
+			$("#weatherDisplay").css('display', 'none');
+		});
+		
     });
